eb7095795888e22e996553d49dfe721861a5a2b2
Terrance Li <terranceli.terbear@gmail.com>
2019-04-13 15:53:17 -0700

    Added state structure to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			name: 'John Doe',
+			balance: 10000, // in dollars, random between 5k and 15k, normal
+			paymentDue: 0,
+			loans: [
+				/* { APR, amountOwed, startTime, minDue } */
+			],
+			monthlyRevenue: 0,
+			stores: [
+				/* { monthlyRent, monthyIncome } */
+			],
+			creditReport: {
+				onTimePayments: 0,
+				missedPayments: 0,
+				avgAge: 0, // loans.reduce((a, c) => a + (month - c.startTime), 0) / numberOfAccounts
+				numberOfAccounts: 0 // loans.length
+			},
+			month: 0 // time counter
+		}
+	}
+
 	render() {
 		return <div className="App" />
 	}
