40bbbe0dd36dd163ea609c72904abec9c53f360d
not simon <fujizhang2525@gmail.com>
2019-04-14 08:52:02 -0700

    dasd

--- src/Store.js
+++ src/Store.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
-
+import Button from './Buttons'
 /**
  * props: {
  *      monthlyCost: Number,
- *      monthlyIncome: Number
+ *      monthlyIncome: Number,
+ *
  * }
  */
 export default class Store extends Component {
 	render() {
-		return <div className="store" />
+		return (
+			<div className="store">
+				<div className="grid">
+					<div className="icon">
+						<img />
+					</div>
+					<h1 className="Name">Name</h1>
+					<div className="stats">a</div>
+					<div className="Actions">
+						<Button type="link">Delete Store</Button>
+						<Button type="link">Upgrade</Button>
+					</div>
+					<div className="income">{this.props.monthlyIncome}</div>
+					<div className="cost">{this.props.monthlyCost}</div>
+				</div>
+			</div>
+		)
 	}
 }
