2fe1724dfdb3fe34afabadf359da6656467b0ec5
Terrance Li <terranceli.terbear@gmail.com>
2019-04-14 06:22:51 -0700

    Notif components

--- src/Notif.js
+++ src/Notif.js
@@ -1,10 +1,11 @@
-import React, {Component} from "react"
+import React, { Component } from 'react'
 
 export default class Notif extends Component {
-  render() {
-    return
-    <div className="notifBox">
-      <h3>some challenge prompt</h3>
-    </div>
-  }
+	render() {
+		return (
+			<div className="notifBox">
+				<h3>{this.props.text}</h3>
+			</div>
+		)
+	}
 }
