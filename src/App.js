a234f88f4a03b68812d2a5063de18ad252aae247
Terrance Li <terranceli.terbear@gmail.com>
2019-04-13 17:02:00 -0700

    Added top-level function signatures

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import React, { Component } from 'react'
 
+const getRandomZipCode = location => {
+	switch (location) {
+		case 2:
+			return ['98101', '02210', '94102', '11215', '30313'][
+				Math.floor(Math.random() * 5)
+			]
+		case 1:
+			return ['98006', '02030', '94010', '07102', '30306'][
+				Math.floor(Math.random() * 5)
+			]
+		default:
+			return ['98844', '02072', '94074', '07083', '30315'][
+				Math.floor(Math.random() * 5)
+			]
+	}
+}
+
 class App extends Component {
 	constructor(props) {
 		super(props)
@@ -21,9 +38,22 @@ class App extends Component {
 				avgAge: 0, // loans.reduce((a, c) => a + (month - c.startTime), 0) / numberOfAccounts
 				numberOfAccounts: 0 // loans.length
 			},
+			location: 0,
 			month: 0 // time counter
 		}
 	}
+
+	getLoans = () => {
+		// call even api
+	}
+
+	takeOutLoan = () => {}
+
+	buyStore = () => {}
+
+	sellStore = i => {}
+
+	nextMonth = () => {}
 
 	render() {
 		return <div className="App" />
