af7daf457a8c3985e23f0a9d714cc34cb61b1628
Terrance Li <terranceli.terbear@gmail.com>
2019-04-14 06:06:36 -0700

    icons

--- src/Store.js
+++ src/Store.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Button from './Button'
-import Building from './images/level_1.png'
+import Level1 from './images/level_1.png'
+import Level2 from './images/level_2.png'
+import Level3 from './images/level_3.png'
 /**
  * props: {
  *      monthlyCost: Number,
@@ -10,6 +12,11 @@ import Building from './images/level_1.png'
  */
 export default class Store extends Component {
 	render() {
+		let image = (c => {
+			if (c < 10000) return Level1
+			if (c < 15000) return Level2
+			return Level3
+		})(this.props.monthlyCost)
 		return (
 			<div
 				className={
@@ -19,7 +26,7 @@ export default class Store extends Component {
 				onClick={this.props.toggleExpand}>
 				<div className="grid">
 					<div className="icon">
-						<img src={Building} alt="building" />
+						<img src={image} alt="building" />
 					</div>
 					<h1 className="Name">Name</h1>
 					<div className="info">a</div>
